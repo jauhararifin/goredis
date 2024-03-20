570351405b83c481401201576a36fe1ea260a6c5
Jauhar Arifin <jauhararifin10@gmail.com>
2024-03-20 21:15:26 +0700

    add benchmark tests

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 
+## Golang Tests
+
+```bash
+# run the tests
+go test . -v -coverprofile=cover.out
+
+# show the coverage
+go tool cover -html cover.out
+
+# run the benchmark tests
+go test . -v -bench=BenchmarkRedisSet -benchmem -benchtime=10s -memprofile=mem.out -cpuprofile=cpu.out -run="^$"
+```
+
 ## Benchmark
 
 redis-benchmark -h localhost -p 3100 -r 100000000000 -P 1000 -c 50 -t SET,GET
--- server_test.go
+++ server_test.go
@@ -1,11 +1,14 @@
 package goredis
 
 import (
+	"bytes"
 	"errors"
 	"io"
 	"log/slog"
+	"math/rand"
 	"net"
 	"os"
+	"sync/atomic"
 	"testing"
 )
 
@@ -125,4 +128,103 @@ func TestServerBootstrap(t *testing.T) {
 		t.Error("after stop complete, all client should be disconnected")
 		return
 	}
+}
+
+// goos: linux
+// goarch: amd64
+// pkg: github.com/jauhararifin/goredis
+// cpu: AMD Ryzen 9 7900X 12-Core Processor
+// BenchmarkRedisSet
+// BenchmarkRedisSet-24             9213043              1349 ns/op            741066 ops/sec           593 B/op         42 allocs/op
+func BenchmarkRedisSet(b *testing.B) {
+	_ = os.Remove(sockfilePath)
+	listener, err := net.Listen("unix", sockfilePath)
+	if err != nil {
+		b.Errorf("cannot open sock file: %s", err.Error())
+		return
+	}
+
+	noopLogger := slog.New(slog.NewTextHandler(io.Discard, nil))
+	server := NewServer(listener, noopLogger)
+
+	go func() {
+		if err := server.Start(); err != nil {
+			b.Errorf("cannot start server: %s", err.Error())
+		}
+	}()
+
+	b.ResetTimer()
+
+	id := atomic.Int64{}
+	b.RunParallel(func(pb *testing.PB) {
+		client, err := net.Dial("unix", sockfilePath)
+		if err != nil {
+			b.Errorf("cannot connect to server: %s", err.Error())
+			b.FailNow()
+			return
+		}
+
+		randomizer := rand.New(rand.NewSource(id.Add(1)))
+
+		pipelineSize := 100
+
+		buff := make([]byte, 4096)
+		writeBuffer := bytes.Buffer{}
+		count := 0
+
+		for pb.Next() {
+			writeBuffer.WriteString("*3\r\n$3\r\nset\r\n$12\r\n")
+			for i := 0; i < 12; i++ {
+				writeBuffer.WriteByte(byte(randomizer.Int31()%96 + 32))
+			}
+			writeBuffer.WriteString("\r\n$12\r\n")
+			for i := 0; i < 12; i++ {
+				writeBuffer.WriteByte(byte(randomizer.Int31()%96 + 32))
+			}
+			writeBuffer.WriteString("\r\n")
+			count++
+
+			if count >= pipelineSize {
+				if _, err := writeBuffer.WriteTo(client); err != nil {
+					b.Errorf("cannot write to server: %s", err.Error())
+					return
+				}
+
+				if _, err := io.ReadFull(client, buff[:5*count]); err != nil {
+					b.Errorf("cannot read from server: %s", err.Error())
+					return
+				}
+
+				count = 0
+			}
+		}
+
+		if count > 0 {
+			if _, err := writeBuffer.WriteTo(client); err != nil {
+				b.Errorf("cannot write to server: %s", err.Error())
+				return
+			}
+
+			if _, err := io.ReadFull(client, buff[:5*count]); err != nil {
+				b.Errorf("cannot read from server: %s", err.Error())
+				return
+			}
+
+			count = 0
+		}
+
+		if err := client.Close(); err != nil {
+			b.Errorf("cannot close client: %s", err.Error())
+			return
+		}
+	})
+	b.StopTimer()
+
+	if err := server.Stop(); err != nil {
+		b.Errorf("cannot stop server: %s", err.Error())
+		return
+	}
+
+	throughput := float64(b.N) / b.Elapsed().Seconds()
+	b.ReportMetric(throughput, "ops/sec")
 }
