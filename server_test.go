1cecacad58feba16cdb1b9f93cb88c50b1376d8d
Jauhar Arifin <jauhararifin10@gmail.com>
2024-03-20 21:05:11 +0700

    test get command

--- server_test.go
+++ server_test.go
@@ -68,4 +68,20 @@ func TestServerBootstrap(t *testing.T) {
 		t.Errorf("calling SET should return 'OK' response, but istead it returns '%s'", string(buff[:nRead]))
 		return
 	}
+
+	if _, err := client.Write([]byte("*2\r\n$3\r\nget\r\n$4\r\nkey1\r\n")); err != nil {
+		t.Errorf("cannot send 'GET key1' to server: %s", err.Error())
+		return
+	}
+
+	nRead, err = io.ReadFull(client, buff[:12])
+	if err != nil {
+		t.Errorf("cannot read server response: %s", err.Error())
+		return
+	}
+
+	if string(buff[:nRead]) != "$6\r\nvalue1\r\n" {
+		t.Errorf("calling GET should return 'value1' response, but istead it returns '%s'", string(buff[:nRead]))
+		return
+	}
 }
